bc97ce0f59560532584f4d08fa094208a4953d58
FrodonSaquet <alexandre.redon@berger-levrault.fr>
2016-01-21 21:22:09 +0100

    test redirect

--- src/covoiturage/bl/servlet/Covoiturage.java
+++ src/covoiturage/bl/servlet/Covoiturage.java
@@ -29,7 +29,7 @@ public class Covoiturage extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.sendRedirect("Index");
+//		response.sendRedirect(request.getContextPath() + "/Index");
 //		this.getServletContext().getRequestDispatcher(VIEW_PAGES_URL).forward( request, response );
 	}
 
